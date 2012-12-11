e753cea29066d869602968da4f45afcd0f8eb8d7
Manuel Guenther <manuel.guenther@idiap.ch>
2012-12-11 15:58:54 +0100

    Changed version to 1.0.0 to upload package to PyPI.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.biosecure',
-    version='1.0.0a1',
+    version='1.0.0',
     description='Biosecure Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.biosecure',
     license='GPLv3',
