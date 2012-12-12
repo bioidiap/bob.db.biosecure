62f6223a647b4d5b37b62f4276b0750de2d66f6a
Manuel Guenther <manuel.guenther@idiap.ch>
2012-12-12 15:16:06 +0100

    Reduced verbose output during database creation.

--- xbob/db/biosecure/create.py
+++ xbob/db/biosecure/create.py
@@ -22,7 +22,7 @@ def add_clients(session, verbose):
                  29,  33,  36,  37,  38,  39,  42,  47,  58,  62,
                  64,  73,  78,  80,  81,  82,  83,  85,  89,  93,
                 102, 104, 107, 108, 109, 111, 112, 117, 119, 123,
-                126, 130, 133, 137, 138, 143, 146, 147, 150, 152, 
+                126, 130, 133, 137, 138, 143, 146, 147, 150, 152,
                 154, 155, 156, 158, 160, 163, 164, 176, 178, 180,
                 183, 196, 198, 199, 200, 201, 203, 206, 209, 210]
   # dev
@@ -43,23 +43,25 @@ def add_clients(session, verbose):
                  125, 128, 132, 139, 140, 144, 148, 149, 151, 161,
                  162, 167, 171, 173, 174, 177, 179, 181, 182, 186,
                  187, 188, 189, 191, 192, 195, 197, 202, 205, 207]
+
+  if verbose: print "Adding clients..."
   groups = [group_world, group_dev, group_eval]
   lists  = [list_world, list_dev, list_eval]
   for k in range(len(groups)):
     g_cur = groups[k]
     l_cur = lists[k]
     for el in l_cur:
-      if verbose: print "Adding client '%d'..." %(el,)
+      if verbose>1: print "  Adding client '%d'..." %(el,)
       session.add(Client(el, g_cur))
 
 def add_files(session, imagedir, verbose):
   """Add files to the Biosecure database."""
- 
+
   def add_file(session, basename, camera, verbose):
     """Parse a single filename and add it to the list.
        Also add a client entry if not already in the database."""
     v = os.path.splitext(basename)[0].split('_')
-    if verbose: print "Adding file '%s'..." %(basename,)
+    if verbose>1: print "  Adding file '%s'..." %(basename,)
     session.add(File(int(v[0][1:4]), os.path.join(camera, basename), int(v[1][2]), camera, int(v[4])))
 
   for camera in filter(nodot, os.listdir(imagedir)):
@@ -67,17 +69,18 @@ def add_files(session, imagedir, verbose):
       continue
 
     camera_dir = os.path.join(imagedir, camera)
+    if verbose: print "Adding files for camera '%s'" % camera
     for filename in filter(nodot, os.listdir(camera_dir)):
       basename, extension = os.path.splitext(filename)
       add_file(session, basename, camera, verbose)
-   
+
 def add_protocols(session, verbose):
   """Adds protocols"""
 
   # 1. DEFINITIONS
   # Numbers in the lists correspond to session identifiers
   protocol_definitions = {}
- 
+
   # Protocol ca0
   enrol = [1, 2]
   probe = [1, 2]
@@ -107,7 +110,7 @@ def add_protocols(session, verbose):
     for key in range(len(protocolPurpose_list)):
       purpose = protocolPurpose_list[key]
       pu = ProtocolPurpose(p.id, purpose[0], purpose[1])
-      if verbose: print "  Adding protocol purpose ('%s','%s')..." % (purpose[0], purpose[1])
+      if verbose>1: print "  Adding protocol purpose ('%s','%s')..." % (purpose[0], purpose[1])
       session.add(pu)
       session.flush()
       session.refresh(pu)
@@ -118,9 +121,9 @@ def add_protocols(session, verbose):
       elif(key == 1 or key == 2): client_group = "dev"
       elif(key == 3 or key == 4): client_group = "eval"
       session_list = []
-      if(key == 0 or key == 1 or key == 3): 
+      if(key == 0 or key == 1 or key == 3):
         session_list = protocol_definitions[proto][0]
-      elif(key == 2 or key == 4): 
+      elif(key == 2 or key == 4):
         session_list = protocol_definitions[proto][1]
 
       # Adds 'protocol' files
@@ -129,15 +132,15 @@ def add_protocols(session, verbose):
               filter(and_(File.session_id == sid, File.camera == p.name)).\
               order_by(File.id)
         for k in q:
-          if verbose: print "    Adding protocol file '%s'..." % (k.path)
+          if verbose>1: print "    Adding protocol file '%s'..." % (k.path)
           pu.files.append(k)
 
 def create_tables(args):
   """Creates all necessary tables (only to be used at the first time)"""
 
-  from bob.db.utils import create_engine_try_nolock 
- 
-  engine = create_engine_try_nolock(args.type, args.files[0], echo=(args.verbose >= 2))
+  from bob.db.utils import create_engine_try_nolock
+
+  engine = create_engine_try_nolock(args.type, args.files[0], echo=(args.verbose > 2))
   Base.metadata.create_all(engine)
 
 # Driver API
@@ -150,7 +153,7 @@ def create(args):
 
   dbfile = args.files[0]
 
-  if args.recreate: 
+  if args.recreate:
     if args.verbose and os.path.exists(dbfile):
       print('unlinking %s...' % dbfile)
     if os.path.exists(dbfile): os.unlink(dbfile)
@@ -160,7 +163,7 @@ def create(args):
 
   # the real work...
   create_tables(args)
-  s = session_try_nolock(args.type, args.files[0], echo=(args.verbose >= 2)) 
+  s = session_try_nolock(args.type, args.files[0], echo=(args.verbose > 2))
   add_clients(s, args.verbose)
   add_files(s, args.imagedir, args.verbose)
   add_protocols(s, args.verbose)
@@ -172,12 +175,8 @@ def add_command(subparsers):
 
   parser = subparsers.add_parser('create', help=create.__doc__)
 
-  parser.add_argument('-R', '--recreate', action='store_true', default=False,
-      help="If set, I'll first erase the current database")
-  parser.add_argument('-v', '--verbose', action='count',
-      help="Do SQL operations in a verbose way")
-  parser.add_argument('-D', '--imagedir', action='store', metavar='DIR',
-      default='/idiap/group/biometric/databases/biosecure/',
-      help="Change the relative path to the directory containing the images of the Biosecure database (defaults to %(default)s)")
-  
+  parser.add_argument('-R', '--recreate', action='store_true', help="If set, I'll first erase the current database")
+  parser.add_argument('-v', '--verbose', action='count', help="Do SQL operations in a verbose way")
+  parser.add_argument('-D', '--imagedir', metavar='DIR', default='/idiap/group/biometric/databases/biosecure/', help="Change the relative path to the directory containing the images of the Biosecure database.")
+
   parser.set_defaults(func=create) #action
