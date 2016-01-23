4fffafcb9a5cc7cfc30f3465d0da1a15970d315a
Andre Anjos <andre.dos.anjos@gmail.com>
2016-01-23 21:43:36 +0100

    [setup] Fix URL making it point to github [ci skip]

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     name='bob.db.biosecure',
     version=version,
     description='Biosecure Database Access API for Bob',
-    url='https://pypi.python.org/pypi/bob.db.biosecure',
+    url='https://github.com/bioidiap/bob.db.biosecure',
     license='GPLv3',
     author='Laurent El Shafey',
     author_email='laurent.el-shafey@idiap.ch',
